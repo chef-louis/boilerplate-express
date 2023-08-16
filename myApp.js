290f5690fd05078217ce01d3db07ac666d1919db
louis.code <louis.code88@gmail.com>
2023-08-16 12:47:25 -0600

    feat: update get request to serve file

--- myApp.js
+++ myApp.js
@@ -2,8 +2,13 @@ let express = require('express');
 let app = express();
 console.log("Hello World");
 
+// app.get('/', function(req, res) {
+//     res.send('Hello Express');
+// })
+
 app.get('/', function(req, res) {
-    res.send('Hello Express');
+    let absolutePath = __dirname + '/views/index.html';
+    res.sendFile(absolutePath);
 })
 
 // RUN LOCAL VERIFICATION BY OPENING "http://localhost:3000" IN BROWSER AFTER RUNNING 'node myApp.js' WITHIN PROJECT DIRECTORY
