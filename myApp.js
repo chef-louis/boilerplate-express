c45fd489ff81f68781631af55d310649460ab7d8
louis.code <louis.code88@gmail.com>
2023-08-16 13:47:23 -0600

    feat: add post handler

--- myApp.js
+++ myApp.js
@@ -48,6 +48,10 @@ app.get('/name', function(req, res) {
     res.json({name: req.query.first + ' ' + req.query.last});
 });
 
+app.post('/name', function(req, res) {
+    res.json({name: req.body.first + ' ' + req.body.last});
+});
+
 
 // RUN LOCAL VERIFICATION BY OPENING "http://localhost:3000" IN BROWSER AFTER RUNNING 'node myApp.js' WITHIN PROJECT DIRECTORY
 app.listen(3000, function() {
