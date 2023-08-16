d660459ace02480e6ed2c81301a4c32671b85a10
louis.code <louis.code88@gmail.com>
2023-08-16 12:52:44 -0600

    feat: add public assets

--- myApp.js
+++ myApp.js
@@ -1,6 +1,7 @@
 let express = require('express');
 let app = express();
-console.log("Hello World");
+app.use('/public', express.static(__dirname + '/public'))
+// console.log("Hello World");
 
 // app.get('/', function(req, res) {
 //     res.send('Hello Express');
