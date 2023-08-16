ca3188be57df713bb2fc9728066fb6f919717d97
louis.code <louis.code88@gmail.com>
2023-08-16 13:22:43 -0600

    feat: add time server middleware

--- myApp.js
+++ myApp.js
@@ -2,23 +2,23 @@ require('dotenv').config();
 const e = require('express');
 let express = require('express');
 let app = express();
-app.use('/public', express.static(__dirname + '/public'))
+app.use('/public', express.static(__dirname + '/public'));
 
 app.use(function(req, res, next) {
     console.log(req.method + ' ' + req.path + ' - ' + req.ip);
     next();
-})
+});
 
 // console.log("Hello World");
 
 // app.get('/', function(req, res) {
 //     res.send('Hello Express');
-// })
+// });
 
 app.get('/', function(req, res) {
     let absolutePath = __dirname + '/views/index.html';
     res.sendFile(absolutePath);
-})
+});
 
 app.get('/json', function(req, res) {
     if (process.env.MESSAGE_STYLE == "uppercase") {
@@ -26,8 +26,14 @@ app.get('/json', function(req, res) {
     } else {
         res.json({"message": "Hello json"});
     }
-})
+});
 
+app.get('/now', function(req, res, next) {
+    req.time = new Date().toString();
+    next();
+}, function(req, res) {
+    res.json({time: req.time});
+});
 
 
 // RUN LOCAL VERIFICATION BY OPENING "http://localhost:3000" IN BROWSER AFTER RUNNING 'node myApp.js' WITHIN PROJECT DIRECTORY
