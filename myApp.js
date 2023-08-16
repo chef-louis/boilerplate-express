aacb111dd0d5a7566eabec8c2ddd2e9c4822683c
louis.code <louis.code88@gmail.com>
2023-08-16 12:44:19 -0600

    feat: add simple get request. add local testing

--- myApp.js
+++ myApp.js
@@ -2,6 +2,16 @@ let express = require('express');
 let app = express();
 console.log("Hello World");
 
+app.get('/', function(req, res) {
+    res.send('Hello Express');
+})
+
+// RUN LOCAL VERIFICATION BY OPENING "http://localhost:3000" IN BROWSER AFTER RUNNING 'node myApp.js' WITHIN PROJECT DIRECTORY
+app.listen(3000, function() {
+    console.log("Server is listening on port 3000...");
+});
+
+
 
 
 
